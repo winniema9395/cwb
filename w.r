48204a3cb40df6a34fdf83ba64d7149ab850e642
winniema9395 <winniema9395@gmail.com>
2017-11-28 20:14:19 +0800

    Signed-off-by: winniema9395 <winniema9395@gmail.com>

---
+++ w.r
@@ -0,0 +1,26 @@
+get_weather=function(FILE,LABEL) {
+  title=c("芠代丁","代溃","溃",
+          "代程蔼溃","代程蔼溃丁","代程溃",
+          "代程溃丁","放","程蔼放","程蔼放丁",
+          "程放","程放丁","臩翴放","癸访",
+          "程癸访","程癸访丁","硉","",
+          "程皚","程皚","程皚硉丁",
+          "秖","计","芠代10だ牧程秖",
+          "芠代10だ牧程癬﹍丁","程秖",
+          "程秖癬﹍丁","ら酚计","ら酚瞯",
+          "ぱら甮秖","ǎ","A籡祇秖")
+  x=read.csv(FILE,skip=1,header=F)
+  names(x) = title; y=x[,c(1,2,8,9,11,14)];
+  y['label']=LABEL; return (y) ;
+}
+sep = get_weather('201709.csv', '201709')
+par(mar=c(5,4,4,2)+1)
+boxplot(sep[,c(3,4,5)],ylab="放" ,xlab="201709", las=1)
+
+aug = get_weather('201709.csv', '201708')
+jul = get_weather('201709.csv', '201707')
+boxplot(aug[,c(3,4,5)],ylab="放" ,xlab="201708", las=1)
+boxplot(jul[,c(3,4,5)],ylab="放" ,xlab="201707", las=1)
+
+plot(aug$代溃, las=1)
+lines(aug$代溃, col="blue")
