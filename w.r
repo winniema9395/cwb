81028f77d14cd5bf8bdfaa5ec251911cd727d5d1
winniema9395 <winniema9395@gmail.com>
2017-12-19 20:22:44 +0800

    Signed-off-by: winniema9395 <winniema9395@gmail.com>

--- w.r
+++ w.r
@@ -9,19 +9,30 @@ get_weather=function(FILE,LABEL) {
           "芠代10だ牧程癬﹍丁","程秖",
           "程秖癬﹍丁","ら酚计","ら酚瞯",
           "ぱら甮秖","ǎ","A籡祇秖")
-  x=read.csv(FILE,skip=1,header=F)
+  x=read.csv(FILE,skip=0,header=F)
   names(x) = title; y=x[,c(1,2,8,9,11,14)];
   y['label']=LABEL; return (y) ;
 }
+jul = get_weather('201707.csv', '201707')
+aug = get_weather('201708.csv', '201708')
 sep = get_weather('201709.csv', '201709')
+oct = get_weather('201710.csv', '201710')
+nov = get_weather('201711.csv', '201711')
+dec = get_weather('201712.csv', '201712')
 par(mar=c(5,4,4,2)+1)
 boxplot(sep[,c(3,4,5)],ylab="放" ,xlab="201709", las=1)
 
-aug = get_weather('201709.csv', '201708')
-jul = get_weather('201709.csv', '201707')
+w2017=rbind(jul,aug,sep,oct,nov,dec)
+table(w2017$label)
+plot(放~label,w2017)
+
+par(mar=c(5,4,4,2)+1)
 boxplot(aug[,c(3,4,5)],ylab="放" ,xlab="201708", las=1)
 boxplot(jul[,c(3,4,5)],ylab="放" ,xlab="201707", las=1)
+boxplot(dec[,c(3,4,5)],ylab="放" ,xlab="201712", las=1)
 
 plot(aug$代溃, las=1)
 lines(aug$代溃, col="blue")
+plot(dec$代溃,type="o",pch=16,col="black",las=1,xlab="
+ら戳",ylab="溃",main="2017/12",axes=TRUE)
 
