f39326986c77cd89c70ad98f9b888996dc189ea8
winniema9395 <winniema9395@gmail.com>
2017-12-26 20:25:37 +0800

    Signed-off-by: winniema9395 <winniema9395@gmail.com>

---
+++ poker.R
@@ -0,0 +1,7 @@
+number = c(1:13,1:13,1:13,1:13)
+number1=c(1:13,4)
+identical(number,number1)
+suit = c(rep("黑桃",13),rep("紅心",13),rep("方塊",13),rep("梅花",13))
+cards = paste(suit,number)
+sample(cards,5)
+
